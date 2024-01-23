6f865f1e2967ed7d56d400e1130da84b98087732
ryukinouu <mahirhassan16@gmail.com>
2024-01-22 20:08:58 -0500

    update global

--- Game/Scripts/Global.gd
+++ Game/Scripts/Global.gd
@@ -12,8 +12,8 @@ func _ready():
 	var angle_step = 2 * PI / CIRCLE_MEASURES
 	for i in range(CIRCLE_MEASURES):
 		var angle = i * angle_step
-		var x = CIRCLE_DIAMETER / 2 * cos(angle)
-		var y = CIRCLE_DIAMETER / 2 * sin(angle)
+		var x = (CIRCLE_DIAMETER - 0.5) / 2 * cos(angle)
+		var y = (CIRCLE_DIAMETER - 0.5) / 2 * sin(angle)
 		positions[i] = {}
 		positions[i]["position"] = Vector3(x, y - 0.1, 0)
 		positions[i]["rotation"] = Vector3(0, 0, rad_to_deg(atan2(y, x)))
