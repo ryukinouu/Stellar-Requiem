a6049defe97dc6c6e6bceb23d982884cc5c360fb
Maggie <maggiemc.chen@mail.utoronto.ca>
2024-02-10 01:41:49 -0500

    "Press any button to begin" UI
    Changed main scene to menu scene and transitioned to tutorial scene after pressing any button

---
+++ Game/Scenes/Main/MainMenu.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed() and not event.echo:
+		# Start the fade-out animation on the ColorRect
+		$CanvasLayer/AnimationPlayer.play("fade_to_black")
+		
+func _on_fade_to_black_finished(animation_name):
+	if animation_name == "fade_to_black":
+	# Change the scene or perform other actions here
+		get_tree().change_scene_to_file("res://Game/Scenes/Levels/Tutorial.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
