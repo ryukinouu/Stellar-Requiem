4a7b5d764edf63e3f6f9d87fa5e4f18c240c024f
ryukinouu <mahirhassan16@gmail.com>
2024-02-16 15:08:51 -0500

    Animation

---
+++ Game/Scenes/Core/Character.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+@onready var anim_tree = $AnimationTree
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_entered(area):
+	pass # Replace with function body.
+
+func _on_area_exited(area):
+	pass # Replace with function body.
