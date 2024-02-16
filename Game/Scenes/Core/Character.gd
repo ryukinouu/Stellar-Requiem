004060ee9596ee4f0680f43af89782f80dfd76bf
ryukinouu <mahirhassan16@gmail.com>
2024-02-16 15:32:05 -0500

    Zorua

--- Game/Scenes/Core/Character.gd
+++ Game/Scenes/Core/Character.gd
@@ -2,15 +2,21 @@ extends Area3D
 
 @onready var anim_tree = $AnimationTree
 
+var current_anim = "Idle"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	anim_tree.active = true
+	Global.cooldown(6,
+			func(): 
+				var state_machine = anim_tree.get("parameters/playback")
+				current_anim = "Walk"
+				state_machine.travel(current_anim)
+	)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_area_entered(area):
 	pass # Replace with function body.
