f3adc021f378119862fce8230ea05f7d1b2467c9
ryukinouu <mahirhassan16@gmail.com>
2024-03-04 12:45:40 -0500

    Plug and Play (works)

--- Game/Framework/UI/Settings.gd
+++ Game/Framework/UI/Settings.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 var input_wait = null
 var txt_btn = null
+var last_btn = null
 
 func _ready():
 	anim_tree.active = true
@@ -12,13 +13,18 @@ func _ready():
 
 func _input(event):
 	if input_wait != null and not event is InputEventMouseMotion:
-		InputMap.action_erase_events(input_wait)
-		InputMap.action_add_event(input_wait, event)
-		if txt_btn:
-			txt_btn.text = event.as_text()
-		input_wait = null
-		txt_btn = null
-		print(event.as_text())
+		if event.is_pressed() and not event.is_echo():
+			InputMap.action_erase_events(input_wait)
+			InputMap.action_add_event(input_wait, event)
+			if txt_btn:
+				txt_btn.get_node("Text").text = event.as_text()
+				last_btn = txt_btn
+			input_wait = null
+			txt_btn = null
+			print(event.as_text())
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			last_btn = null
 
 func _on_texture_button_pressed():
 	var state_machine = anim_tree.get("parameters/playback")
@@ -31,21 +37,22 @@ func _on_audio_stream_player_finished():
 	$AudioStreamPlayer.play()
 
 func _on_move_left_1_pressed():
-	set_input_wait("ui_left", $Control/Player1/left/Button/Text)
+	set_input_wait("left", $Control/Player1/left/Button)
 
 func _on_move_right_1_pressed():
-	set_input_wait("ui_right", $Control/Player1/right/Button/Text)
+	set_input_wait("right", $Control/Player1/right/Button)
 
 func _on_action_1_pressed():
-	set_input_wait("ui_accept", $Control/Player1/action/Button/Text)
+	set_input_wait("action", $Control/Player1/action/Button)
 
 func _on_pause_1_pressed():
-	set_input_wait("ui_cancel", $Control/Player1/escape/Button/Text)
+	set_input_wait("escape", $Control/Player1/escape/Button)
 
 func set_input_wait(action_name: String, button: Control):
-	input_wait = action_name
-	txt_btn = button
-	txt_btn.text = "Press any key..."
+	if last_btn != button:
+		input_wait = action_name
+		txt_btn = button
+		txt_btn.get_node("Text").text = "Press any key..."
 
 func _on_texture_button_toggled(toggled_on):
 	$AudioStreamPlayer.stream_paused = toggled_on
