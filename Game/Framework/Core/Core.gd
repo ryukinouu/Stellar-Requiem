7075bae59f867061e374ad210640120948baf588
Aryaman Arora <rowan.arora@mail.utoronto.ca>
2024-03-19 02:27:05 -0400

    Hopefully last commit for Beta Version

--- Game/Framework/Core/Core.gd
+++ Game/Framework/Core/Core.gd
@@ -6,7 +6,7 @@ func cooldown(time, exe):
 
 var data = {
 	"apollo": true,
-	"artemis": false,
+	"artemis": true,
 	"d_lives": 3,
 	"g_lives": 3,
 	"current_score": 0,
--- Game/Framework/RYM-Engine/LevelHandler.gd
+++ Game/Framework/RYM-Engine/LevelHandler.gd
@@ -11,10 +11,15 @@ extends Node3D
 @onready var d_lane_top = $Main/Notes/d_top
 @onready var d_lane_bottom = $Main/Notes/d_bottom
 @onready var d_lane_right = $Main/Notes/d_right
+@onready var g_lane_left = $Main/Notes/g_left
+@onready var g_lane_top = $Main/Notes/g_top
+@onready var g_lane_bottom = $Main/Notes/g_bottom
+@onready var g_lane_right = $Main/Notes/g_right
 
 @onready var apollo = $Main/Apollo
 @onready var apollo_animtree = $Main/Apollo/AnimationTree
 @onready var artemis = $Main/Artemis
+@onready var artemis_animtree = $Main/Artemis/AnimationTree
 
 @onready var apollo_v = $"Main/Player1-Visuals"
 @onready var artemis_v = $"Main/Player2-Visuals"
@@ -35,7 +40,12 @@ extends Node3D
 @export var wav_delay : float = 6.0
 @export var music_first : bool = false
 
-var note_scene = load("res://Game/Scenes/Notes/Note.tscn")
+var d_note_scene = load("res://Game/Scenes/Notes/Note.tscn")
+var g_green_note_scene = load("res://Game/Scenes/Notes/GreenNote.tscn")
+var g_red_note_scene = load("res://Game/Scenes/Notes/RedNote.tscn")
+var g_yellow_note_scene = load("res://Game/Scenes/Notes/YellowNote.tscn")
+var g_blue_note_scene = load("res://Game/Scenes/Notes/BlueNote.tscn")
+
 var apollo_afterimage = load("res://Game/Scenes/Core/Apollo_Afterimage.tscn")
 var miss_texture = load("res://Assets/Textures/Indicators/MISS.png")
 var great_texture = load("res://Assets/Textures/Indicators/GREAT!.png")
@@ -47,13 +57,57 @@ var mapping = {
 	36: "d_left",
 	38: "d_top",
 	40: "d_bottom",
-	41: "d_right"
+	41: "d_right",
+	
+	12: "g_left",
+	19: "g_top",
+	24: "g_bottom",
+	31: "g_right",
+	
+	13: "g_left",
+	20: "g_top",
+	25: "g_bottom",
+	32: "g_right",
+	
+	14: "g_left",
+	21: "g_top",
+	26: "g_bottom",
+	33: "g_right",
+	
+	15: "g_left",
+	22: "g_top",
+	27: "g_bottom",
+	34: "g_right"
 }
 var canhit = {
 	"d_left": [], 
 	"d_top": [], 
 	"d_bottom": [], 
-	"d_right": []
+	"d_right": [],
+	"g_left": {
+		"green": [],
+		"red": [],
+		"yellow": [],
+		"blue": []
+	},
+	"g_top": {
+		"green": [],
+		"red": [],
+		"yellow": [],
+		"blue": []
+	},
+	"g_bottom": {
+		"green": [],
+		"red": [],
+		"yellow": [],
+		"blue": []
+	},
+	"g_right": {
+		"green": [],
+		"red": [],
+		"yellow": [],
+		"blue": []
+	}
 }
 var note_values = {
 	"miss": 0,
@@ -76,14 +130,18 @@ var char_lanes = {
 		"left": -9,
 		"top": -17,
 		"bottom": -25,
-		"right": -3
+		"right": -33
 	},
 }
 
-var artemis_boost_speed = 50.0
-var artemis_move_speed = 15.0
+var artemis_boost_speed = 22.0
+var artemis_move_speed = 10.0
 var artemis_initial_speed = artemis_boost_speed
 var artemis_tween
+var artemis_instant_left = false
+var artemis_next_left
+var artemis_instant_right = false
+var artemis_next_right
 
 var delta_stellar = 0.05
 var delta_great = 0.05
@@ -109,6 +167,14 @@ func get_lane(direction):
 		return d_lane_bottom
 	elif direction == "d_right":
 		return d_lane_right
+	elif direction == "g_left":
+		return g_lane_left
+	elif direction == "g_top":
+		return g_lane_top
+	elif direction == "g_bottom":
+		return g_lane_bottom
+	elif direction == "g_right":
+		return g_lane_right
 
 func loading(out):
 	if out:
@@ -167,7 +233,7 @@ func _ready():
 		$GUI/HUD/Side2.visible = true
 		$GUI/HUD/Score/Upper/Player2.visible = true
 		
-		anim.track_set_key_value(tcam_pos, kcam_pos, Vector3(0, 20, -16))
+		anim.track_set_key_value(tcam_pos, kcam_pos, Vector3(0, 25, -20))
 		Core.cooldown(2, func():
 			var tween = get_tree().create_tween()
 			tween.tween_property(
@@ -291,9 +357,24 @@ func _on_note_event(channel, event):
 			if event.note in mapping.keys():
 				# SPAWN
 				var note_direction = mapping[event.note]
-				if note_direction.substr(0, 2) == "d_" and apollo_notes_disabled:
-					return
-				
+				var note_scene
+				if note_direction.substr(0, 2) == "d_":
+					if apollo_notes_disabled:
+						return
+					note_scene = d_note_scene
+				elif note_direction.substr(0, 2) == "g_":
+					if artemis_notes_disabled:
+						return
+					
+					if event.note == 12 or event.note == 19 or event.note == 24 or event.note == 31:
+						note_scene = g_green_note_scene
+					elif event.note == 13 or event.note == 20 or event.note == 25 or event.note == 32:
+						note_scene = g_red_note_scene
+					elif event.note == 14 or event.note == 21 or event.note == 26 or event.note == 33:
+						note_scene = g_yellow_note_scene
+					elif event.note == 15 or event.note == 22 or event.note == 27 or event.note == 34:
+						note_scene = g_blue_note_scene
+
 				var note_instance = note_scene.instantiate()
 				var mesh_instance_paths = ["Sphere", "Sphere_001", "Sphere_002", "Sphere_003", "Sphere_004"]
 				for mesh_instance_path in mesh_instance_paths:
@@ -321,11 +402,19 @@ func _on_note_event(channel, event):
 				
 				# HIT WINDOW
 				Core.cooldown(2 - hit_delta, func():
-					canhit[note_direction].append(note_instance)
+					var note_color = null
+					if event.note <= 34:
+						note_color = get_note_color(event.note)
+						canhit[note_direction][note_color].append(note_instance)
+					else:
+						canhit[note_direction].append(note_instance)
 					tweens[note_instance] = tween
 					Core.cooldown(hit_delta + delta_miss, func():
 						if note_instance in canhit[mapping[event.note]]:
 							canhit[mapping[event.note]].erase(note_instance)
+							tweens.erase(note_instance)
+						elif note_color != null and note_instance in canhit[mapping[event.note]][note_color]:
+							canhit[mapping[event.note]][note_color].erase(note_instance)
 							tweens.erase(note_instance)
 					)
 				)
@@ -348,6 +437,16 @@ func _on_note_event(channel, event):
 						)
 					)
 				)
+
+func get_note_color(note):
+	if note == 12 or note == 19 or note == 24 or note == 31:
+		return "green"
+	elif note == 13 or note == 20 or note == 25 or note == 32:
+		return "red"
+	elif note == 14 or note == 21 or note == 26 or note == 33:
+		return "yellow"
+	elif note == 15 or note == 22 or note == 27 or note == 34:
+		return "blue"
 
 func change_indicator(note, old, new):
 	if note:
@@ -396,8 +495,9 @@ func _input(event):
 	if paused or !can_pause:
 		return
 	
+	# Apollo Movement/Action Buttons
 	if !apollo_notes_disabled:
-		if event.is_action_pressed("action-top-1"):
+		if event.is_action_pressed("apollo-top"):
 			if char_lanes["apollo"]["current"] != "top":
 				apollo_afterimage_effect()
 				char_lanes["apollo"]["current"] = "top"
@@ -408,7 +508,7 @@ func _input(event):
 			if canhit["d_top"].size() > 0:
 				var note = canhit["d_top"].pop_front()
 				note_on_hit(note)
-		elif event.is_action_pressed("action-bottom-1"):
+		elif event.is_action_pressed("apollo-bottom"):
 			if char_lanes["apollo"]["current"] != "bottom":
 				apollo_afterimage_effect()
 				char_lanes["apollo"]["current"] = "bottom"
@@ -419,7 +519,7 @@ func _input(event):
 			if canhit["d_bottom"].size() > 0:
 				var note = canhit["d_bottom"].pop_front()
 				note_on_hit(note)
-		elif event.is_action_pressed("action-left-1"):
+		elif event.is_action_pressed("apollo-left"):
 			if char_lanes["apollo"]["current"] != "left":
 				apollo_afterimage_effect()
 				char_lanes["apollo"]["current"] = "left"
@@ -430,7 +530,7 @@ func _input(event):
 			if canhit["d_left"].size() > 0:
 				var note = canhit["d_left"].pop_front()
 				note_on_hit(note)
-		elif event.is_action_pressed("action-right-1"): 
+		elif event.is_action_pressed("apollo-right"): 
 			if char_lanes["apollo"]["current"] != "right":
 				apollo_afterimage_effect()
 				char_lanes["apollo"]["current"] = "right"
@@ -441,9 +541,62 @@ func _input(event):
 			if canhit["d_right"].size() > 0:
 				var note = canhit["d_right"].pop_front()
 				note_on_hit(note)
-	
+
+	# Artemis Single Movement
 	if !artemis_notes_disabled:
-		if event.is_action_pressed("left") or event.is_action_pressed("right"):
+		if event is InputEventKey:
+			if event.keycode == KEY_LEFT:
+				if event.pressed and not event.echo:
+					if char_lanes["artemis"]["current"] != "left":
+						if char_lanes["artemis"]["current"] == "top":
+							artemis_next_left = char_lanes["artemis"]["left"]
+						elif char_lanes["artemis"]["current"] == "bottom":
+							artemis_next_left = char_lanes["artemis"]["top"]
+						elif char_lanes["artemis"]["current"] == "right":
+							artemis_next_left = char_lanes["artemis"]["bottom"]
+						artemis_instant_left = true
+				else:
+					artemis_instant_left = false
+			elif event.keycode == KEY_RIGHT:
+				if event.pressed and not event.echo:
+					if char_lanes["artemis"]["current"] != "right":
+						if char_lanes["artemis"]["current"] == "bottom":
+							artemis_next_right = char_lanes["artemis"]["right"]
+						elif char_lanes["artemis"]["current"] == "top":
+							artemis_next_right = char_lanes["artemis"]["bottom"]
+						elif char_lanes["artemis"]["current"] == "left":
+							artemis_next_right = char_lanes["artemis"]["top"]
+						artemis_instant_right = true
+				else:
+					artemis_instant_right = false
+
+		elif event is InputEventJoypadButton:
+			if event.button_index == JOY_BUTTON_DPAD_UP:
+				if event.pressed:
+					if char_lanes["artemis"]["current"] != "left":
+						if char_lanes["artemis"]["current"] == "top":
+							artemis_next_left = char_lanes["artemis"]["left"]
+						elif char_lanes["artemis"]["current"] == "bottom":
+							artemis_next_left = char_lanes["artemis"]["top"]
+						elif char_lanes["artemis"]["current"] == "right":
+							artemis_next_left = char_lanes["artemis"]["bottom"]
+						artemis_instant_left = true
+				else:
+					artemis_instant_left = false
+			elif event.button_index == JOY_BUTTON_DPAD_DOWN:
+				if event.pressed:
+					if char_lanes["artemis"]["current"] != "right":
+						if char_lanes["artemis"]["current"] == "bottom":
+							artemis_next_right = char_lanes["artemis"]["right"]
+						elif char_lanes["artemis"]["current"] == "top":
+							artemis_next_right = char_lanes["artemis"]["bottom"]
+						elif char_lanes["artemis"]["current"] == "left":
+							artemis_next_right = char_lanes["artemis"]["top"]
+						artemis_instant_right = true
+				else:
+					artemis_instant_right = false
+		# Artemis Continuous Movement
+		if event.is_action_pressed("artemis-left") or event.is_action_pressed("artemis-right"):
 			if artemis_tween and artemis_tween.is_running():
 				artemis_tween.kill()
 			artemis_tween = get_tree().create_tween()
@@ -452,11 +605,56 @@ func _input(event):
 				func():
 					artemis_boost_speed = artemis_move_speed
 			)
-		elif event.is_action_released("left") or event.is_action_released("right"):
+			if event.is_action_pressed("artemis-left"):
+				artemis_animtree.get("parameters/playback").travel("Tilt_L")
+			else:
+				artemis_animtree.get("parameters/playback").travel("Tilt_R")
+		elif event.is_action_released("artemis-left") or event.is_action_released("artemis-right"):
 			if artemis_tween:
 				artemis_tween.kill()
 			if char_lanes["artemis"]["current"] != "left" and char_lanes["artemis"]["current"] != "right":
 				artemis_boost_speed = artemis_initial_speed
+			artemis_animtree.get("parameters/playback").travel("Forward")
+		# Artemis Action Buttons
+		elif event.is_action_pressed("artemis-green"):
+			var curr_lane = get_curr_canhit_lane()
+			artemis_animtree.get("parameters/playback").travel("Hit_001")
+			if canhit[curr_lane]["green"].size() > 0:
+				var note = canhit[curr_lane]["green"].pop_front()
+				note_on_hit(note)
+		elif event.is_action_pressed("artemis-red"):
+			var curr_lane = get_curr_canhit_lane()
+			artemis_animtree.get("parameters/playback").travel("Hit_002")
+			if canhit[curr_lane]["red"].size() > 0:
+				var note = canhit[curr_lane]["red"].pop_front()
+				note_on_hit(note)
+		elif event.is_action_pressed("artemis-yellow"):
+			var curr_lane = get_curr_canhit_lane()
+			artemis_animtree.get("parameters/playback").travel("Hit_003")
+			if canhit[curr_lane]["yellow"].size() > 0:
+				var note = canhit[curr_lane]["yellow"].pop_front()
+				note_on_hit(note)
+		elif event.is_action_pressed("artemis-blue"):
+			var curr_lane = get_curr_canhit_lane()
+			artemis_animtree.get("parameters/playback").travel("Hit_004")
+			if canhit[curr_lane]["blue"].size() > 0:
+				var note = canhit[curr_lane]["blue"].pop_front()
+				note_on_hit(note)
+		elif event.is_action_released("artemis-green") or event.is_action_released("artemis-red") \
+		or event.is_action_released("artemis-yellow") or event.is_action_released("artemis-blue"):
+			artemis_animtree.get("parameters/playback").travel("Forward")
+				
+func get_curr_canhit_lane():
+	var curr_lane = char_lanes["artemis"]["current"]
+	if curr_lane == "left":
+		return "g_left"
+	elif curr_lane == "top":
+		return "g_top"
+	elif curr_lane == "bottom":
+		return "g_bottom"
+	elif curr_lane == "right":
+		return "g_right"
+	return "Error"
 
 func _process(delta):
 	var progress_ratio = $GUI/HUD/Score/Bar.value / $GUI/HUD/Score/Bar.max_value
@@ -467,14 +665,20 @@ func _process(delta):
 	$GUI/HUD/SoloScore2/Text.text = str("%07d" % snapped(Core.data["current_score"], 1))
 	$GUI/HUD/Score/Upper/Score.text = str("%07d" % snapped(Core.data["current_score"], 1))
 	if can_pause:
-		if Input.is_action_pressed("left"):
+		if Input.is_action_pressed("artemis-left"):
 			if artemis.position.x < -9:
-				artemis.position.x = lerp(artemis.position.x, artemis.position.x + 0.1 * artemis_boost_speed, delta * artemis_move_speed)
-		if Input.is_action_pressed("right"):
+				if artemis_instant_left:
+					artemis.position.x = lerp(artemis.position.x, float(artemis_next_left), delta * (artemis_move_speed + 5))
+				else:
+					artemis.position.x = lerp(artemis.position.x, artemis.position.x + 0.1 * artemis_boost_speed, delta * artemis_move_speed)
+		if Input.is_action_pressed("artemis-right"):
 			if artemis.position.x > -33:
-				artemis.position.x = lerp(artemis.position.x, artemis.position.x - 0.1 * artemis_boost_speed, delta * artemis_move_speed)
+				if artemis_instant_right:
+					artemis.position.x = lerp(artemis.position.x, float(artemis_next_right), delta * (artemis_move_speed + 5))
+				else:
+					artemis.position.x = lerp(artemis.position.x, artemis.position.x - 0.1 * artemis_boost_speed, delta * artemis_move_speed)
 		
-		if not Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		if not Input.is_action_pressed("artemis-left") and not Input.is_action_pressed("artemis-right"):
 			var to_left = abs(artemis.position.x - char_lanes["artemis"]["left"])
 			var to_top = abs(artemis.position.x - char_lanes["artemis"]["top"])
 			var to_bottom = abs(artemis.position.x - char_lanes["artemis"]["bottom"])
@@ -483,12 +687,16 @@ func _process(delta):
 			var target_x
 			if closest_pos == to_left:
 				target_x = char_lanes["artemis"]["left"]
+				char_lanes["artemis"]["current"] = "left"
 			elif closest_pos == to_top:
 				target_x = char_lanes["artemis"]["top"]
+				char_lanes["artemis"]["current"] = "top"
 			elif closest_pos == to_bottom:
 				target_x = char_lanes["artemis"]["bottom"]
+				char_lanes["artemis"]["current"] = "bottom"
 			elif closest_pos == to_right:
 				target_x = char_lanes["artemis"]["right"]
+				char_lanes["artemis"]["current"] = "right"
 			artemis.position.x = lerp(artemis.position.x, float(target_x), delta * artemis_move_speed)
 
 func _on_settings_pressed():
@@ -561,3 +769,4 @@ func _on_music_finished():
 			loading(true)
 		)
 	)
+	
